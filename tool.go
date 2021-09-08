16105d44ad08c6b4d55213676dfa33485ff2b121
imchuncai <imchuncai@gmail.com>
2021-09-08 17:05:47 +0800

    upd

--- tool.go
+++ tool.go
@@ -9,18 +9,52 @@ import (
 	"google.golang.org/grpc"
 )
 
-func ErrorInterceptor(logger log.Logger) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler) (resp interface{}, err error) {
-		defer func() { err = doRecover(logger) }()
-		return handler(ctx, req)
+var _logger log.Logger
+
+func SetLogger(logger log.Logger) {
+	_logger = logger
+}
+
+func chekcLogger() {
+	if _logger == nil {
+		panic("grpctool: logger is not set")
 	}
 }
 
-func doRecover(logger log.Logger) error {
-	if err := recover(); err != nil {
-		logger.Log(log.Error, err, string(debug.Stack()))
-		return errors.New("server response an error")
+func ErrorInterceptor() grpc.ServerOption {
+	chekcLogger()
+	return grpc.UnaryInterceptor(
+		func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+			handler grpc.UnaryHandler) (resp interface{}, err error) {
+			defer func() {
+				if err != nil {
+					return
+				}
+				if err := recover(); err != nil {
+					_logger.Log(log.Error, err, string(debug.Stack()))
+					err = errors.New("server response an error")
+				}
+			}()
+			return handler(ctx, req)
+		})
+}
+
+func Must(err error) {
+	if err != nil {
+		panic(err)
 	}
-	return nil
+}
+
+func MustForInit(err error) {
+	chekcLogger()
+	if err == nil {
+		return
+	}
+	_logger.Log(log.Error, err, debug.Stack())
+	panic(err)
+}
+
+func Log(prefix log.Prefix, v ...interface{}) {
+	chekcLogger()
+	_logger.Log(prefix, v...)
 }
